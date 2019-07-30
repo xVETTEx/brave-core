65ae4e09bf0a6b0deb6053885318ede8a615e413
Simon Hong <shong@igalia.com>
2019-07-31 01:09:39 +0900

    Implement brave playlists backend
    This PR implements playlists extension api for managing playlists
    from extensions or webui.
    
    - PlaylistsController is core controller that supports create/get/delete
    - playlist and each PlaylistsController is owned by PlaylistsService.
    - PlaylistsController manages playlists db and assets via
    - PlaylistsMediaFileController and PlaylistsDBController.
    - Playlists is managed per profile.
    - update to latest youtubedown.js
    - lots of robustness checks in playlist controller, real types for TypeScript glue code, update glue with support for separate video and audio files
    - sync PlaylistType
    - override shields and adblock page to demo playlists
    - fix PlaylistInfo type with separate video and audio path
    - add audio and video elements to the playlist table

--- components/brave_extension/extension/brave_extension/containers/braveShields.tsx
+++ components/brave_extension/extension/brave_extension/containers/braveShields.tsx
@@ -7,86 +7,37 @@ import * as React from 'react'
 // CSS normalizer
 import 'emptykit.css'
 
-// Components group
-import AdvancedView from './advancedView'
-import SimpleView from './simpleView'
-import ReadOnlyView from './readOnlyView'
-
-// API
-import * as shieldsAPI from '../background/api/shieldsAPI'
-
-// Types
-import { Tab, PersistentData } from '../types/state/shieldsPannelState'
-import {
-  ShieldsToggled,
-  ReportBrokenSite,
-  BlockAdsTrackers,
-  HttpsEverywhereToggled,
-  BlockJavaScript,
-  BlockFingerprinting,
-  BlockCookies,
-  AllowScriptOriginsOnce,
-  SetScriptBlockedCurrentState,
-  SetGroupedScriptsBlockedCurrentState,
-  SetAllScriptsBlockedCurrentState,
-  SetFinalScriptsBlockedState,
-  SetAdvancedViewFirstAccess,
-  ShieldsReady
-} from '../types/actions/shieldsPanelActions'
-import { SetStoreSettingsChange } from '../types/actions/settingsActions'
-import { SettingsData } from '../types/other/settingsTypes'
-
-// Helpers
-import { shieldsHasFocus } from '../helpers/shieldsUtils'
-
-interface Props {
-  actions: {
-    shieldsToggled: ShieldsToggled
-    reportBrokenSite: ReportBrokenSite
-    blockAdsTrackers: BlockAdsTrackers
-    httpsEverywhereToggled: HttpsEverywhereToggled
-    blockJavaScript: BlockJavaScript
-    blockFingerprinting: BlockFingerprinting
-    blockCookies: BlockCookies
-    allowScriptOriginsOnce: AllowScriptOriginsOnce
-    setScriptBlockedCurrentState: SetScriptBlockedCurrentState
-    setGroupedScriptsBlockedCurrentState: SetGroupedScriptsBlockedCurrentState
-    setAllScriptsBlockedCurrentState: SetAllScriptsBlockedCurrentState
-    setFinalScriptsBlockedState: SetFinalScriptsBlockedState
-    setAdvancedViewFirstAccess: SetAdvancedViewFirstAccess
-    setStoreSettingsChange: SetStoreSettingsChange
-    shieldsReady: ShieldsReady
-  }
-  shieldsPanelTabData: Tab
-  persistentData: PersistentData
-  settingsData: SettingsData
-}
-
 interface State {
-  showReadOnlyView: boolean
+  url: string
 }
 
 export default class Shields extends React.PureComponent<Props, State> {
   constructor (props: Props) {
     super(props)
     this.state = {
-      showReadOnlyView: false
+      url: ''
     }
   }
 
-  toggleReadOnlyView = () => {
-    this.setState({ showReadOnlyView: !this.state.showReadOnlyView })
-  }
-
-  toggleAdvancedView = () => {
-    const { showAdvancedView } = this.props.settingsData
-    shieldsAPI.setViewPreferences({ showAdvancedView: !showAdvancedView })
-      .then(() => this.props.actions.setStoreSettingsChange({ showAdvancedView: !showAdvancedView }))
-      .catch((err) => console.log('[Shields] Unable to toggle advanced view interface:', err))
-  }
-
   componentDidMount () {
-    this.props.actions.shieldsReady()
+    this.getActiveTabUrl()
+  }
+
+  get pageHasDownloadableVideo () {
+    return this.state.url.startsWith('https://www.youtube.com/watch')
+  }
+
+  getActiveTabUrl = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const activeTab = tabs[0]
+      if (activeTab.url) {
+        this.setState({ url: activeTab.url })
+      }
+    })
+  }
+
+  onClickDownloadVideo = (url: string) => {
+    chrome.bravePlaylists.requestDownload(url)
   }
 
   componentDidUpdate (prevProps: Props) {
@@ -99,33 +50,16 @@ export default class Shields extends React.PureComponent<Props, State> {
   }
 
   render () {
-    const { shieldsPanelTabData, persistentData, settingsData, actions } = this.props
-    const { showReadOnlyView } = this.state
-    if (!shieldsPanelTabData) {
-      return null
-    }
-    return settingsData.showAdvancedView
+    const { url } = this.state
+
+    return this.pageHasDownloadableVideo
       ? (
-        <AdvancedView
-          shieldsPanelTabData={shieldsPanelTabData}
-          persistentData={persistentData}
-          toggleAdvancedView={this.toggleAdvancedView}
-          actions={actions}
-        />
-      ) : showReadOnlyView
-      ? (
-        <ReadOnlyView
-          shieldsPanelTabData={shieldsPanelTabData}
-          toggleReadOnlyView={this.toggleReadOnlyView}
-        />
+        <div>
+          <h1>This page has a video you can download</h1>
+          <button onClick={this.onClickDownloadVideo.bind(this, url)}>Click here to download</button>
+        </div>
       ) : (
-        <SimpleView
-          shieldsPanelTabData={shieldsPanelTabData}
-          persistentData={persistentData}
-          actions={actions}
-          toggleAdvancedView={this.toggleAdvancedView}
-          toggleReadOnlyView={this.toggleReadOnlyView}
-        />
+        <h1>Nothing to see here. Go to a YT video to see the magic</h1>
       )
   }
 }
