47345daa7ad8e35e88f1d72eb935bc52d29892ba
mkarolin <max@brave.com>
2020-05-28 20:01:18 -0400

    WIP: ContentSettingsAgentImpl moved to //components.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/af3597cb7e26db8909a4883bd528efb84c6d7ce2
    
    commit af3597cb7e26db8909a4883bd528efb84c6d7ce2
    Author: Clark DuVall <cduvall@chromium.org>
    Date:   Wed Apr 15 19:20:24 2020 +0000
    
        Move ContentSettingsAgentImpl to //components/content_settings
    
        This splits chrome-specific logic into
        ChromeContentSettingsAgentDelegate, and moves common logic into
        //components/content_settings. This will allow sharing the content
        settings agent with WebLayer.
    
        The unit test for WhitelistedSchemes was moved into the browser tests,
        which means we no longer need a static version of
        IsWhitelistedForContentSettings() just for testing.
    
        Bug: 1065537

---
+++ chromium_src/third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -0,0 +1,52 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/components/content_settings/renderer/brave_content_settings_agent_impl_helper.h"
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_RETURN                           \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return;
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_NULLPTR                          \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return nullptr;
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_NULLOPT                          \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return base::nullopt;
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_ZERO                             \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return 0;
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_MINUS_ONE                        \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return -1;
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_SCRIPT_VALUE                     \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return ScriptValue::CreateNull(script_state->GetIsolate());
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_GET_EXTENSION                      \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) { \
+    v8::Local<v8::Value> wrapped_extension =                                  \
+        ToV8(extension, script_state->GetContext()->Global(),                 \
+             script_state->GetIsolate());                                     \
+    return ScriptValue(script_state->GetIsolate(), wrapped_extension);        \
+  }
+
+#define BRAVE_WEBGL_RENDERING_CONTEXT_BASE_STRING                           \
+  if (canvas() && !AllowFingerprinting(canvas()->GetDocument().GetFrame())) \
+    return String();
+
+#include "../../../../../../third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc.cc"
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_STRING
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_GET_EXTENSION
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_SCRIPT_VALUE
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_MINUS_ONE
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_ZERO
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_NULLOPT
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_NULLPTR
+#undef BRAVE_WEBGL_RENDERING_CONTEXT_BASE_RETURN
--- components/content_settings/renderer/brave_content_settings_agent_impl.cc
+++ components/content_settings/renderer/brave_content_settings_agent_impl.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/renderer/brave_content_settings_agent_impl.h"
+#include "brave/components/content_settings/renderer/brave_content_settings_agent_impl.h"
 
 #include <string>
 #include <utility>
@@ -32,6 +32,7 @@
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "url/url_constants.h"
 
+namespace content_settings {
 namespace {
 
 GURL GetOriginOrURL(
@@ -366,3 +367,5 @@ bool BraveContentSettingsAgentImpl::AllowAutoplay(bool default_value) {
                "ContentSettingsAgentImpl::AllowAutoplay says so";
   return allow;
 }
+
+}  // namespace content_settings
